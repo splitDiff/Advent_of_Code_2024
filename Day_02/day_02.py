2819403ccd4f11e73da8ab310c7b929114430c87
John <yzwy4mty9f@snkmail.com>
2024-12-02 14:44:55 -0500

    Clean up Part 2

--- Day_02/day_02.py
+++ Day_02/day_02.py
@@ -1,5 +1,4 @@
 import collections
-debug = False
 
 def GetData(filename):
     reports = []
@@ -13,7 +12,6 @@ def GetData(filename):
 def Part1(reports):
     safe_reports = 0
     for report in reports:
-        # print(report)
         safe_report = True
         last_read = report[0]
         if report[1] == report[0]:
@@ -27,7 +25,6 @@ def Part1(reports):
             if sign != first_sign:
                 safe_report = False
             last_read = read
-            # print(f"{read=}; {sign=}; {safe_report=}")
 
         last_read = report[0]
         for read in report[1:]:
@@ -35,7 +32,6 @@ def Part1(reports):
             if delta < 1 or delta > 3:
                 safe_report = False
             last_read = read
-            # print(f"{read=}; {delta=}; {safe_report=}")
 
         if safe_report:
             safe_reports += 1
@@ -50,7 +46,6 @@ def CheckSign(report):
         if sign != first_sign:
             return False
         last_read = read
-        if debug: print(f"Checksign {read=} {last_read=}")
     return True
 
 
@@ -61,38 +56,24 @@ def CheckDelta(report):
         if delta < 1 or delta > 3:
             return False
         last_read = read
-        if debug: print(f"CheckDelta {read=} {last_read=} {delta=}")
     return True
 
 
 def Part2(reports):
-    good = open(".\\Day_02\\good_reports.txt","w")
-    bad = open(".\\Day_02\\bad_reports.txt","w")
 
     safe_reports = 0
     for report in reports:
         original_report = report
-        good_report = True
-        good_report = good_report and CheckDelta(report) and CheckSign(report)
-        if good_report:
-            good.write(f"{report}\n")
+        if CheckDelta(report) and CheckSign(report):
             safe_reports += 1
         else:
             for i, num in enumerate(report):
                 rep = report[0:i] + report[i+1:]
-                good_report = CheckDelta(rep) and CheckSign(rep)
-                if good_report:
-                    if debug: print(f"{report} passed on {i} with {rep}")
-                    good.write(f"{report} corrected to {rep}\n")
+                if CheckDelta(rep) and CheckSign(rep):
                     safe_reports += 1
                     break;
-            if not good_report: bad.write(f"{report}\n")
 
-    good.close()
-    bad.close()
     return safe_reports
-
-
 
 reports = GetData("./Day_02/input.txt")
 print(f"Part 1 result: {Part1(reports)}")
